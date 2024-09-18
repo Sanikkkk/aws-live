6262f2100bbd6a505df9cebf888a241bbb9f3821
Sanikkkk <114151472+Sanikkkk@users.noreply.github.com>
2024-09-18 21:11:53 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.c5iig1dquarc.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "sanikyeh123"
+customdb = "employee"
+custombucket = "chanyuhan-employee"
+customregion = "us-east-1"
+
